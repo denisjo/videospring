b4c94906faed3ae9eec254705e9aaa75ba79a911
denisjo <jonathan.denis@jdlabs.be>
2016-08-03 21:39:54 +0200

    Test

--- config/routes.js
+++ config/routes.js
@@ -11,7 +11,5 @@ export default (
   <Route path="/" component={App}>
     <Route path="/viewcontract/:contractNumber" component={ViewContractController} />
     <Route path="/createcontract/:poolCode/:candReference" component={CreateContractController} />
-    
-    
   </Route>
 );
